c099af1b05a9293822d269e7d0d643fb467ae206
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-22 18:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function validEmail(str) {
+function validEmail(s) {
   //your JS code here.
+
+	let n=s.length;
+
+	if(n==0)
+		return false;
+
+	
+	
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const/ str = prompt("Enter an email address.");
 alert(validEmail(str));
